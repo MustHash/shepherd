38337b7ff991352dbaac553aaf561949ac620a35
Couto <luiscouto@triworks.net>
2013-05-02 15:22:49 +0100

    Fix error reporting

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ middleware.whitelist = function (ips) {
             next();
         } else {
             resp.statusCode = 403;
-            next('Remote address doesn\'t match whitelisted addresses.');
+            resp.end('Remote address doesn\'t match whitelisted addresses.');
         }
     };
 };
@@ -27,33 +27,31 @@ middleware.whitelist = function (ips) {
 middleware.payload = function () {};
 
 
-var cdir = function (ip, suffix) {
+var cdir = function (suffix) {
+
     var ones = new Array(suffix + 1).join(1).split(''),
         zeros = new Array((32 - suffix) + 1).join(0).split(''),
         bits = ones.concat(zeros),
         address = '';
 
     while (bits.length) {
-        console.log(bits);
         address += parseInt(bits.splice(0, 8).join(''), 2);
-        address += '.';
-        console.log(address);
+        if (bits.length) { address += '.'; }
     }
 
     return address;
+
 };
 
+module.exports = function (opts) {
 
-server = connect()
-    .use(connect.logger())
-    .use(connect.bodyParser())
-    .use(connect.errorHandler())
-    .use(middleware.whitelist(['127.0.0.1']))
-    .use(function (req, resp, next) {
-        resp.end();
-    });
+    var server = connect()
+        .use(connect.logger())
+        .use(connect.bodyParser())
+        .use(connect.errorHandler())
+        .use(middleware.whitelist(opts.whitelist))
+        .use(function (req, resp, next) { resp.end(); });
 
-module.exports = function (options) {
-    console.log(options)
     return http.createServer(server);
 };
+
